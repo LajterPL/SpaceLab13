47ee5011b6ab947038c02301f949a29a85a42b8b
WhiterPL <whiterphone@gmail.com>
2022-10-09 12:02:57 +0200

    + Wyświetlanie regionu na panelu gry

--- core/src/io/github/whiterpl/sl13/gui/GamePane.java
+++ core/src/io/github/whiterpl/sl13/gui/GamePane.java
@@ -4,18 +4,62 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import io.github.whiterpl.sl13.atoms.Region;
+import io.github.whiterpl.sl13.atoms.Tile;
 
 public class GamePane extends Table {
 
-    protected Label gameLabel;
+    private Label gameScreen;
 
     public GamePane(BitmapFont font, NinePatchDrawable border) {
         Label.LabelStyle labelStyle = new Label.LabelStyle();
         labelStyle.font = font;
-        gameLabel = new Label("34x48", labelStyle);
-        gameLabel.setWrap(true);
+        gameScreen = new Label("34x48", labelStyle);
+        gameScreen.setWrap(true);
 
-        this.add(gameLabel).expand().center().left().pad(5);
+        this.add(gameScreen).expand().center().left().pad(12);
         this.background(border);
+    }
+
+    public void updateGameScreen(Region activeRegion, int x, int y) {
+
+        x = x < 16 ? 16 : Math.min(x, Region.WIDTH - 17);
+        y = y < 18 ? 18 : Math.min(y, Region.HEIGHT - 18);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int iY = y - 18; iY < y + 18; iY++) {
+            for (int iX = x - 18; iX < x + 17; iX++) {
+                Tile temp = activeRegion.getTile(iX, iY);
+                if (temp == null) sb.append(" ");
+                else {
+                    if (temp.getMob() != null) {
+                        sb.append("[#");
+                        sb.append(temp.getMob().getColorString());
+                        sb.append(']');
+                        sb.append(temp.getMob().getSymbol());
+                        sb.append("[]");
+                    } else if (temp.getStructure() != null) {
+                        sb.append("[#");
+                        sb.append(temp.getStructure().getColorString());
+                        sb.append(']');
+                        sb.append(temp.getStructure().getSymbol());
+                        sb.append("[]");
+                    } else if (!temp.getItems().isEmpty()) {
+                        sb.append("[#");
+                        sb.append(temp.getItems().get(0).getColorString());
+                        sb.append(']');
+                        sb.append(temp.getItems().get(0).getSymbol());
+                        sb.append("[]");
+                    } else {
+                        sb.append(" ");
+                    }
+                }
+            }
+            if (iY != y + 17) sb.append("\n");
+        }
+
+        gameScreen.setText(sb.toString());
+
     }
 }
--- core/src/io/github/whiterpl/sl13/gui/GameStage.java
+++ core/src/io/github/whiterpl/sl13/gui/GameStage.java
@@ -5,25 +5,27 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import io.github.whiterpl.sl13.atoms.Region;
 
 public class GameStage extends Stage {
+    private GamePane gamePane;
+    private InfoPane infoPane;
 
     public GameStage (BitmapFont guiFont, BitmapFont gamefont, NinePatchDrawable border) {
         Table rootTable = new Table();
         rootTable.setFillParent(true);
         rootTable.center();
-        rootTable.setDebug(true);
         this.addActor(rootTable);
 
-        GamePane gamePane = new GamePane(gamefont, border);
+        gamePane = new GamePane(gamefont, border);
         rootTable.add(gamePane).expandY().width(Gdx.graphics.getHeight()).fill().top().left();
 
-        InfoPane infoPane = new InfoPane(guiFont, border);
+        infoPane = new InfoPane(guiFont, border);
         rootTable.add(infoPane).expand().fill().top().left().padLeft(5);
     }
 
-    @Override
-    public void draw() {
-        super.draw();
+    public void updateGameScreen(Region acticeRegion, int x, int y) {
+        gamePane.updateGameScreen(acticeRegion, x, y);
     }
+
 }
