28aab6923303ec1c37f164552f6c1f24e5c25fbb
WhiterPL <whiterphone@gmail.com>
2022-11-04 12:24:40 +0100

    Enum do kierunków ruchu

---
+++ core/src/io/github/whiterpl/sl13/atoms/Direction.java
@@ -0,0 +1,13 @@
+package io.github.whiterpl.sl13.atoms;
+
+public enum Direction {
+    NORTH,
+    NORTH_EAST,
+    EAST,
+    SOUTH_EAST,
+    SOUTH,
+    SOUTH_WEST,
+    WEST,
+    NORTH_WEST,
+    NONE
+}
