2826cfc5fee88d6ac33ad535641ee29771dffec2
WhiterPL <whiterphone@gmail.com>
2022-11-04 12:24:58 +0100

    Klasa do tworzenia regionów

---
+++ core/src/io/github/whiterpl/sl13/atoms/RegionGenerator.java
@@ -0,0 +1,23 @@
+package io.github.whiterpl.sl13.atoms;
+
+public class RegionGenerator {
+
+    public static Region getTestingRegion() {
+        Region region = new Region();
+        Structure wall = new Structure("Wall", "Wall", '#', "9e9baaff");
+        wall.statuses.add(Status.BLOCK_PASSING);
+
+        for (int x = 0; x < Region.WIDTH; x++) {
+            region.tiles[x][0].setStructure(wall);
+            region.tiles[x][Region.HEIGHT - 1].setStructure(wall);
+        }
+
+        for (int y = 0; y < Region.WIDTH; y++) {
+            region.tiles[0][y].setStructure(wall);
+            region.tiles[Region.WIDTH - 1][y].setStructure(wall);
+        }
+
+        return region;
+    }
+
+}
