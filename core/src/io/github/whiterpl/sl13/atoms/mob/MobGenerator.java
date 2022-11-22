56ffaec7a824e59eb2451c8847b8620558b51bf7
WhiterPL <whiterphone@gmail.com>
2022-11-22 11:13:52 +0100

    Klasa do generowania mobów

---
+++ core/src/io/github/whiterpl/sl13/atoms/mob/MobGenerator.java
@@ -0,0 +1,28 @@
+package io.github.whiterpl.sl13.atoms.mob;
+
+import com.badlogic.gdx.Gdx;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MobGenerator {
+
+    private static final Random randomGen = new Random();
+
+    public static String getRandomHumanName() {
+        BufferedReader rd = new BufferedReader(new InputStreamReader(Gdx.files.internal("strings/randomnames.txt").read()));
+        ArrayList<String> names = new ArrayList<>();
+        rd.lines().forEach(names::add);
+        return names.get(randomGen.nextInt(names.size()));
+    }
+
+    public static short calculateMaxHp(short toughness) {
+        return (short) (50 + toughness * 10);
+    }
+
+    public static short calculateMaxSp(short willpower) {
+        return (short) (50 + willpower * 10);
+    }
+}
