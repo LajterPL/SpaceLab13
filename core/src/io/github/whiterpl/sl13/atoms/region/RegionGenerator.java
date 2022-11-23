a5e14ba99aeba3f155fbc5b2bb12502e6f9a9b53
WhiterPL <whiterphone@gmail.com>
2022-11-23 11:27:26 +0100

    Moby do testowania szybkości

--- core/src/io/github/whiterpl/sl13/atoms/region/RegionGenerator.java
+++ core/src/io/github/whiterpl/sl13/atoms/region/RegionGenerator.java
@@ -1,6 +1,7 @@
 package io.github.whiterpl.sl13.atoms.region;
 
 import io.github.whiterpl.sl13.atoms.Status;
+import io.github.whiterpl.sl13.atoms.mob.Mob;
 import io.github.whiterpl.sl13.atoms.structure.Structure;
 
 public class RegionGenerator {
@@ -20,6 +21,17 @@ public class RegionGenerator {
             region.tiles[Region.WIDTH - 1][y].setStructure(wall);
         }
 
+        Mob testMob = new Mob("Alien", "ayy lmao", 'A', "FF00FFFF");
+        testMob.setActionDelay(7);
+        testMob.placeMob(region, 10, 20);
+
+        Mob testMob2 = new Mob("Salamander", "Some kind of a lizard", 'S', "efe51cFF");
+        testMob2.setActionDelay(9);
+        testMob2.placeMob(region, 9, 20);
+
+        Mob testMob3 = new Mob("Zombie", "He wants your brain", 'Z', "1cef6aFF");
+        testMob3.setActionDelay(15);
+        testMob3.placeMob(region, 8, 20);
         return region;
     }
 
