68892d55484ef9c36dc0c800567b601422fc097b
WhiterPL <whiterphone@gmail.com>
2022-11-23 11:30:32 +0100

    Metoda do losowania kierunku

--- core/src/io/github/whiterpl/sl13/atoms/Direction.java
+++ core/src/io/github/whiterpl/sl13/atoms/Direction.java
@@ -1,6 +1,9 @@
 package io.github.whiterpl.sl13.atoms;
 
+import java.util.Random;
+
 public enum Direction {
+
     NORTH,
     NORTH_EAST,
     EAST,
@@ -9,5 +12,21 @@ public enum Direction {
     SOUTH_WEST,
     WEST,
     NORTH_WEST,
-    NONE
+    NONE;
+
+    private static final Random randomGen = new Random();
+
+    public static Direction getRandomDirection() {
+        switch (randomGen.nextInt(9)) {
+            case 0: return NORTH;
+            case 1: return NORTH_EAST;
+            case 2: return EAST;
+            case 3: return SOUTH_EAST;
+            case 4: return SOUTH;
+            case 5: return SOUTH_WEST;
+            case 6: return WEST;
+            case 7: return NORTH_WEST;
+            default: return NONE;
+        }
+    }
 }
