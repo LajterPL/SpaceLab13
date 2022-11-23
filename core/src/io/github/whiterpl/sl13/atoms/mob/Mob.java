69c4f6d8e829002fe52bd9a141ab04e04a0ceee4
WhiterPL <whiterphone@gmail.com>
2022-11-23 11:29:40 +0100

    Manager akcji dla mobów i szkielet wykonywania akcji

--- core/src/io/github/whiterpl/sl13/atoms/mob/Mob.java
+++ core/src/io/github/whiterpl/sl13/atoms/mob/Mob.java
@@ -1,16 +1,16 @@
 package io.github.whiterpl.sl13.atoms.mob;
 
-import com.badlogic.gdx.Gdx;
 import io.github.whiterpl.sl13.atoms.*;
 import io.github.whiterpl.sl13.atoms.item.Item;
 import io.github.whiterpl.sl13.atoms.region.Region;
 import io.github.whiterpl.sl13.atoms.region.Tile;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Stream;
 
 public class Mob extends Atom {
+
+    protected int detectionRange;
 
     protected short maxHp;
     protected short currentHp;
@@ -30,16 +30,28 @@ public class Mob extends Atom {
     protected int actionDelay;
     protected int nextUpdateTurn;
 
-    public Mob(String name, String description, char symbol, String colorString, Status... statuses) {
+    private ActionManager actionManager;
+
+
+    public Mob(String name, String description, char symbol, String colorString, int detectionRange, Status... statuses) {
         super(name, description, symbol, colorString, statuses);
         this.equipment = new ArrayList<>();
         this.skills = new short[9];
+        this.detectionRange = detectionRange;
+        this.actionManager = new ActionManager();
     }
 
-    public Mob(String name, String description, char symbol, String colorString) {
+    public Mob(String name, String description, char symbol, String colorString, int detectionRange, int actionDelay) {
         super(name, description, symbol, colorString);
         this.equipment = new ArrayList<>();
         this.skills = new short[9];
+        this.detectionRange = detectionRange;
+        this.actionManager = new ActionManager();
+        this.actionDelay = actionDelay;
+    }
+
+    public Mob(String name, String description, char symbol, String colorString) {
+        this(name, description, symbol, colorString, 20, 10);
     }
 
     // GETTERS & SETTERS
@@ -120,12 +132,24 @@ public class Mob extends Atom {
         return actionDelay;
     }
 
+    public void setActionDelay(int actionDelay) {
+        this.actionDelay = actionDelay;
+    }
+
     public int getNextUpdateTurn() {
         return nextUpdateTurn;
     }
 
     public void setNextUpdateTurn(int nextUpdateTurn) {
         this.nextUpdateTurn = nextUpdateTurn;
+    }
+
+    public int getDetectionRange() {
+        return detectionRange;
+    }
+
+    public void setDetectionRange(int detectionRange) {
+        this.detectionRange = detectionRange;
     }
 
     // METHODS
@@ -184,12 +208,15 @@ public class Mob extends Atom {
         if (selectedTile.blocksPassage()) return false;
 
         Mob tempMob = null;
-        if (selectedTile.getMob() != null) tempMob = selectedTile.getMob();
+        if (selectedTile.getMob() != null) {
+            tempMob = selectedTile.getMob();
+            selectedTile.setMob(null);
+            tempMob.setX(this.x);
+            tempMob.setY(this.y);
+        }
 
-        region.getTile(this.x, this.y).setMob(null);
+        region.getTile(this.x, this.y).setMob(tempMob);
         selectedTile.setMob(this);
-
-        if (tempMob != null) region.getTile(this.x, this.y).setMob(tempMob);
 
         this.x = this.x + xMod;
         this.y = this.y + yMod;
@@ -197,5 +224,30 @@ public class Mob extends Atom {
         return true;
     }
 
+    public void act(Region region) {
+        Stream<Action> possibleActions = actionManager.getActions(this, region);
 
+        //TODO: Filtrowanie akcji, które są możliwe i dodawanie/odejmowanie priorytetu zależnie od tego, czy coś przeszkadza
+        possibleActions
+                .max(Comparator.comparingInt(Action::getPriority))
+                .ifPresentOrElse(
+                        action -> {
+                            switch (action.getActionResponse()) {
+                                case ATTACK:
+
+                                case WALK:
+                                    move(region, Direction.SOUTH);
+                                    //TODO: Pathfinding
+                                case LOOT:
+                            }
+                        },
+                        () -> {
+
+                            move(region, Direction.NORTH);
+                        }        );
+    }
+
+    public void addDelay() {
+        this.nextUpdateTurn += actionDelay;
+    }
 }
