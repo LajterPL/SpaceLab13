3543b1965b9b94e91680b11e14828fa6dec33dc8
WhiterPL <whiterphone@gmail.com>
2023-02-02 22:04:38 +0100

    Inicjalizacja gry i lista poziomów

--- core/src/io/github/whiterpl/sl13/Game.java
+++ core/src/io/github/whiterpl/sl13/Game.java
@@ -7,28 +7,57 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
+import io.github.whiterpl.sl13.atoms.item.ItemGenerator;
+import io.github.whiterpl.sl13.atoms.mob.MobGenerator;
+import io.github.whiterpl.sl13.atoms.region.Region;
+import io.github.whiterpl.sl13.atoms.region.RegionGenerator;
+import io.github.whiterpl.sl13.atoms.structure.StructureGenerator;
 import io.github.whiterpl.sl13.gui.InfoPane;
 import io.github.whiterpl.sl13.gui.StageSwapper;
 import io.github.whiterpl.sl13.player.PlayerController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game extends ApplicationAdapter {
 
 	private static Game gameInstance;
 
+	//GUI
 	private BitmapFont guiFont;
 	private BitmapFont gameFont;
 	private NinePatchDrawable terminalBorder;
 	private StageSwapper stageSwapper;
 
+	//GAME
+	private List<Region> levels;
+
+
 	public static Game getInstance() {
 		if (gameInstance == null) gameInstance = new Game();
 		return gameInstance;
 	}
+
 	public static PlayerController getPlayerController() {
 		return gameInstance.stageSwapper.getPlayerController();
 	}
 	public static InfoPane getInfoPane() { return getInstance().stageSwapper.getGameStage().getInfoPane();}
 	public static StageSwapper getStageSwapper() { return getInstance().stageSwapper;}
+	public static List<Region> getLevels() { return getInstance().levels;}
+
+	public void startNewGame() {
+		levels.clear();
+
+		MobGenerator.initialize();
+		ItemGenerator.initialize();
+		StructureGenerator.initialize();
+
+		levels.add(RegionGenerator.getStartingRegion());
+
+		getPlayerController().setActiveRegion(levels.get(0));
+	}
+
+
 
 	@Override
 	public void create () {
@@ -44,6 +73,7 @@ public class Game extends ApplicationAdapter {
 
 		stageSwapper = new StageSwapper(guiFont, gameFont, terminalBorder);
 
+		levels = new ArrayList<>();
 	}
 
 	@Override
